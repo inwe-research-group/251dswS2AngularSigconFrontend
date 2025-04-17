ec8a27d7584975130121a6578f506a489244d556
inwe-research-group <zmamanir@unmsm.edu.pe>
2025-04-16 20:35:00 -0500

    add metodos al Personaservice

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { IPersonaResponse } from '../model/persona-response';
 import { BASE_URL } from '../utils/constants';
 import { Observable } from 'rxjs';
+import { IPersonaRequest } from '../model/persona-request';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,16 @@ export class PersonaService {
   constructor(private http: HttpClient) {}
   getPersonas(): Observable<IPersonaResponse[]> {
     return this.http.get<IPersonaResponse[]>(`${BASE_URL}/persona`);
+  }
+  registrarPersona(persona: IPersonaRequest): Observable<IPersonaResponse> {
+    return this.http.post<IPersonaResponse>(`${BASE_URL}/persona`, persona);
+  }
+  eliminarPersona(persona: IPersonaRequest): Observable<IPersonaResponse> {
+    return this.http.delete<IPersonaResponse>(`${BASE_URL}/persona`, {
+      body: persona,
+    });
+  }
+  actualizarPersona(persona: IPersonaRequest): Observable<IPersonaResponse> {
+    return this.http.put<IPersonaResponse>(`${BASE_URL}/persona`, persona);
   }
 }
