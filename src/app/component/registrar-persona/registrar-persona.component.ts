d8e173053eec0b97a0655f0588d5d62c0e0fec54
inwe-research-group <zmamanir@unmsm.edu.pe>
2025-04-16 19:26:35 -0500

    add getPersonas to registrar-persona

--- src/app/component/registrar-persona/registrar-persona.component.ts
+++ src/app/component/registrar-persona/registrar-persona.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IPersonaResponse } from '../../model/persona-response';
+import { PersonaService } from '../../service/persona.service';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-persona',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './registrar-persona.component.html',
   styleUrl: './registrar-persona.component.css',
 })
-export class RegistrarPersonaComponent {}
+export class RegistrarPersonaComponent {
+  personaArray: IPersonaResponse[] = [];
+  personaForm: FormGroup;
+  constructor(private personaService: PersonaService) {
+    this.personaForm = new FormGroup({
+      idPersona: new FormControl(''),
+      apellidoPaterno: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      apellidoMaterno: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      nombres: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+    });
+  }
+  ngOnInit(): void {
+    this.getPersonas();
+  }
+  getPersonas(): void {
+    this.personaService.getPersonas().subscribe((result: any) => {
+      console.log('Result', result);
+      this.personaArray = result;
+    });
+  }
+}
